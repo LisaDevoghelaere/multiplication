614eb2ea89e8efb850b20600ed21977f4957c25a
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-05-29 17:26:44 +0200

    test php

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>Multiplication</title>
 </head>
 <body>
-    
+    <!--Page 1 - Afficher la table de multiplication de 3-->
+    <?php
+    $i=3;
+    Echo 'Table de multiplication de 3<br>';
+    for ($j = 1; $j <= 10; $j++)
+    {
+        echo $i.' x '.$j.' = '.$i*$j.'<br>';
+    }
+
+    ?>
 </body>
 </html>
