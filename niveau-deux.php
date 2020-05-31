5e6ad127a11ac1ee0292f7cd9e2b8c35f7ddf2a6
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-05-31 12:06:09 +0200

    ajout page 2

---
+++ niveau-deux.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niveau 2</title>
+</head>
+<body>
+    <form action="">
+        <select name="quelle-table" id="quelle-table">
+            <option value="un" id="un">table de 1</option>
+            <option value="deux" id="deux">table de 2</option>
+            <option value="trois" id="trois">table de 3</option>
+            <option value="quatre" id="quatre">table de 4</option>
+            <option value="cinq" id="cinq">table de 5</option>
+            <option value="six" id="six">table de 6</option>
+            <option value="sept" id="sept">table de 7</option>
+            <option value="huit" id="huit">table de 8</option>
+            <option value="neuf" id="neuf">table de 9</option>
+            <option value="dix"id="dix">table de 10</option>
+        </select>
+    </form>
+</body>
+</html>
