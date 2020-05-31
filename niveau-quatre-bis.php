3ab8e76c12340d2b26a95dfa715ef786771a559c
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-05-31 14:53:42 +0200

    structure 4bis et 6

---
+++ niveau-quatre-bis.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niveau 4 BIS</title>
+</head>
+<body>
+    <!--•	Une fois l'étape 4 finalisée : intégrer AJAX au mode révision afin de rendre l'utilisation du mode révision plus fluide.-->
+</body>
+</html>
---
+++ niveau-six.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niveau 6</title>
+</head>
+<body>
+    <!--Page 6 - Une fois l'étape 5 finalisée : intégrer AJAX au mode révision afin de rendre l'utilisation du mode révision plus fluide.-->
+</body>
+</html>
