9d305d030e23fe5f2601338159d6467aa0d42f61
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-06-04 09:15:44 +0200

    $resultat=$i*$j;

--- niveau-quatre.php
+++ niveau-quatre.php
@@ -22,6 +22,56 @@
             </ul>
         </nav>
     </header>
-    <!--mode révision : l'utilisateur choisit une table de multiplication et doit répondre à une multiplication tirée au hasard dans la table sélectionnée. -->
+    <!--mode révision : l'utilisateur choisit une table de multiplication et doit répondre à une multiplication tirée au hasard dans la table sélectionnée. 
+    L'utilisateur choisi un multiplicande et le multiplicateur est choisit par un random de un à 10
+    input pour répondre à la question si la réponse est conforme à i*j "bien joué" sinon "raté, la réponse est : i*j "-->
+    <main>
+        <h2>Choisis une table à réviser</h2>
+        <p>Sélectionne le chiffre de la table que tu veux réviser et trouve la réponse à la multiplication qui va apparaitre</p>
+        <form method="post" name="table_de_multiplication" action="">
+            <select name="choix">
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+            <input type="submit" name="valider" value="OK"/>
+        </form>
+        <p>Combien font... </p>
+        <?php
+        if(isset($_POST['choix'])||isset($_POST["reponse"])){
+            $i = $_POST['choix'];
+            $j=random_int(0,10);
+            echo $i . " x " . $j . " = " . "?";
+        ?>
+        <form action="" method="post">
+            <input type="text" name="reponse">
+            <input type="submit" value="Valider">
+        </form>
+        <?php
+         
+            if(isset($_POST['reponse'])){
+                $resultat=$i*$j;
+                if ($_POST["reponse"]==$resultat){
+                ?>
+                        <p>Bravo! Tu as trouvé!</p>
+                    <?php
+                    }
+                else{
+                    ?>
+                        <p>Mauvaise Réponse!</p>
+                    <?php
+                    }
+            }
+        }
+        ?>
+    </main>
 </body>
 </html>
